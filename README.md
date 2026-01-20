4c11774751ea5c3aebd28fdcb8342ee7f14cafec
ken-kaneki-9 <160580260+ken-kaneki-9@users.noreply.github.com>
2026-01-20 14:17:33 +0530

    The grammar is properly being enforced

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ This is implementation of python3 interpreter but in C
 
 Interpreter is software that executes source code without first compiling it to machine code.(https://en.wikipedia.org/wiki/Interpreter_(computing))
 
+# Grammar
+Text->Number->Operator->Number->Operator...so on.
+>This grammar tells the first value can't be operator and can only be a number.
+
+**Parse**-This function is only called when the *i* is zero, it check the current token type is zero or not. If zero continue and if not zero throw a error
+
+![Screenshot](images/image.png)
+
 # Design Patterns
 
 **fgets**- I choose this for taking input as there are multiple ways this seems the most bare bone to me. It was quite easy to implement and also stores to the character array automatically.
@@ -24,4 +32,5 @@ typedef struct {
   2. 1 represent an Character
 
 ![Screenshot](images/unnamed.png)
+
 
--- arithmetic_interpreter.c
+++ arithmetic_interpreter.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <string.h>
 #define INPUT_LENGTH 1000
 #define INTEGER 0
@@ -13,13 +14,21 @@ Token current_token;//global variable with no initialisation
 bool is_digit(char c){
   return c>='0'&& c<='9';
 }
+void parse(int type){
+    if(current_token.type!=type){
+      printf("Exected a integer got a operator");
+      exit(-1);
+    }
+}
 void identifying_type(char raw_token){
   if(is_digit(raw_token)){
     current_token.type=INTEGER;
     current_token.number_value=raw_token-'0';
+    // printf("type=%d and value=%d\n",current_token.type,current_token.number_value);
   }else{
     current_token.type=OPERATOR;
     current_token.operator_value=raw_token;
+    // printf("type=%d and value=%c\n",current_token.type,current_token.operator_value);
   }
   
 }
@@ -28,6 +37,9 @@ void interpreter(char *text){
   int length=strlen(text);
   for(int i=0;i<length-1;i++){//Here -1 is for \n given by fgets
     identifying_type(text[i]);//Here I need to identify wether the given char is a number or a operator
+    if (i==0){
+    parse(INTEGER);  
+    }
   }
 }
 int main(){
